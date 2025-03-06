e30e5cfebeb53d3693bb0bb59b310a63b88879f1
Raphaël/ <izoret@proton.me>
2025-03-06 14:57:53 +0100

    seracrawl leretour

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
 import NavBar from './components/NavBar.vue'
 import Footer from './components/Footer.vue'
+import Seracrawl from "./components/Seracrawl.vue";
 </script>
 
 <template>
   <div class="min-h-screen flex flex-col">
-    <NavBar />
+    <NavBar/>
     <main class="flex-grow">
       <router-view></router-view>
     </main>
-    <Footer />
+
+    <Seracrawl/>
+
+    <Footer/>
   </div>
 </template>
-
-<style>
-
-</style>
---
+++ src/components/Seracrawl.vue
@@ -0,0 +1,32 @@
+<template>
+  <aside>
+    <img src="/img/seracrawl.png" alt="Publicité">
+    <p>image alt</p>
+  </aside>
+</template>
+
+<style scoped>
+aside {
+  position: fixed;
+  right: 12px;
+  top: 150px;
+  width: 250px;
+  background-color: #f9f9f9;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  padding: 10px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  z-index: 0;
+}
+
+aside img {
+  width: 100%;
+  border-radius: 4px;
+}
+
+aside p {
+  font-size: 0.9rem;
+  color: #555;
+  text-align: center;
+}
+</style>
